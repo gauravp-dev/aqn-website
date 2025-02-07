7187d5f96a84de73fafcf8872c955a902e452a2f
Yogesh_Mishra <yogesh32y@gmail.com>
2025-02-08 03:39:15 +0530

    update changes

--- src/app/landing-page/components/newfeat.tsx
+++ src/app/landing-page/components/newfeat.tsx
@@ -3,20 +3,20 @@ import Image from "next/image";
 
 function Newfeat() {
   return (
-    <div className="h-[90%] flex flex-col justify-center items-center py-12">
+    <div className="min-h-screen flex flex-col justify-center items-center">
       <h1 className="text-center text-2xl md:text-3xl font-semibold font-montserrat text-[#0B4D6B] mb-8">
         Meet Aquapro, Our end-to-end ship
         <br />
         management platform.
       </h1>
 
-      <div className="flex-1 flex justify-center items-center w-full h-[60%]">
+      <div className="flex-1 flex justify-center items-center w-full">
         <Image 
           src="newfeat.svg" 
           alt="My Logo" 
           width={100} 
           height={100} 
-          className="w-[90%] h-[70%]"
+          className="w-[90%] h-auto"
         />
       </div>
     </div>

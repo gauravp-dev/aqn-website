f8314bc7de834b7a5aa782f27c0fff0e3e38e0cf
Yogesh_Mishra <yogesh32y@gmail.com>
2025-02-10 21:23:10 +0530

    hover page added in navbar

--- src/app/landing-page/page.tsx
+++ src/app/landing-page/page.tsx
@@ -3,7 +3,7 @@
 import ProductShowcase from "@/app/landing-page/components/product";
 import Hero from "./components/hero";
 import "../globals.css";
-import AquaproFeatures from "./components/features";
+
 import Feature2 from "./components/feature2";
 import About from "./components/about";
 import Contact from "./components/contact";
@@ -11,21 +11,17 @@ import SecurityAndBackup from "./components/securityAndBackup";
 import Footer from "./components/footer";
 import Newfeat from "./components/newfeat";
 
-
 export default function Home() {
   return (
     <>
       <Hero />
       <ProductShowcase />
-      <AquaproFeatures/>
-      <Feature2/>
-      <About/>
-      <Contact/>
-      <SecurityAndBackup/>
-      <Footer/>
-      <Newfeat/>
-      
-   
+      <Newfeat />
+      <Feature2 />
+      <About />
+      <Contact />
+      <SecurityAndBackup />
+      <Footer />
     </>
   );
 }
